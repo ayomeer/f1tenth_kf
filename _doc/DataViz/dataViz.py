c449ee7ab6128956a78aea2ffe765d516d2e8107
Andreas Kuhn <ayomeer@gmail.com>
2024-05-17 16:27:01 +0200

    perfect measurement synch, but some weirdness in how I got there

--- _doc/DataViz/dataViz.py
+++ _doc/DataViz/dataViz.py
@@ -5,43 +5,40 @@ from dataViz_utils import *
 
 
 # --- Config -----------------------------------------------------------------------------
+# Measurement data paths
+rosbag_path = 'ROS/sensordata_05_path'
+optitrack_csv_path = 'OptiTrack/gt_OptiTrack_05_path.csv'
 
-
-rosbag_path = 'ROS/sensordata_05_circle_left'
-optitrack_csv_path = 'OptiTrack/gt_OptiTrack_05_circle_left.csv'
-
-
-# toggle plots
+# Toggle plots
 PLOT_POS_GT_RAW = 0
 PLOT_POS_GT_ADJ = 1
 
-PLOT_POS_ODOM = 0
+PLOT_POS_ODOM = 1
 
 PLOT_ODOM_ORIENTATIONS = 0
 PLOT_IMU_ORIENTATIONS = 1
 
-# --- Read Data -----------------------------------------------------------------------------
+PLOT_STARTPOINT_DETECTION = 0
 
+
+# --- Read Data -----------------------------------------------------------------------------
 # Read Pose from RosBag
 pos_odom, orient_odom = readOdomData(rosbag_path)
 
 # Read IMU Data from RosBag
-theta_imu_dot, pos_imu_dot = readImuData(rosbag_path) # message in deg/s even though doc says rad/s
+theta_imu_dot, lin_accel_imu = readImuData(rosbag_path) # message in deg/s even though doc says rad/s
 theta_imu_dot = deg2rad(theta_imu_dot) 
-# NOTE: pos_imu_dot in IMU reference frame
+# NOTE: lin_accel_imu in IMU reference frame
 
 # Read OptiTrack ground truth from CSV
 pos_gt, orient_gt = readOptiTrackCSV(optitrack_csv_path)
 
 
 # --- Data Manipulation ----------------------------------------------------------------
-
 # --- Postion Data ---
-
 # Odom
-# Reset Odom. if multiple measurements made without restarting driver stack
-pos_odom -= pos_odom[0]
-
+pos_odom -= pos_odom[0] # Reset Odom 
+                        # (in case multiple measurements made without restarting driver stack)
 q0 = orient_odom[0]
 q0[-1] = -q0[-1] # rotate the other way (back)
 pos_odom = quaternionRotPoints(pos_odom, q0)
@@ -56,14 +53,15 @@ pos_gt -= pos_gt[gt_startFrame]
 # Align  gt with odom starting orientation (odom is reference for car)
 q0 = orient_gt[gt_startFrame]
 q0[-1] = -q0[-1] # rotate the other way (back)
-pos_gt_ = quaternionRotPoints(pos_gt, q0)
+pos_gt_aligned = quaternionRotPoints(pos_gt, q0)
 
-print("Aligning gt to odom with q0=",q0)
-print("Rotation axis vector: ", getQuatAxis(q0))
-print("q0 rotation in deg: ", quat2yaw(q0)*(360)/(2*np.pi))
-
+print("--- Coord system matching -------------------------------------------- ")
+print("Aligning gt to odom with q0= \t",q0)
+print("Rotation axis unit vector: \t", getQuatAxis(q0))
+print("q0 rotation in deg: \t\t", round(quat2yaw(q0)*(360)/(2*np.pi),5))
 
 # --- Orientation Data ---
+orient_dir_0 = np.array([1,0,0]) # base orientation vector to transform onto paths
 
 # Odom
 # get subsampled versions of odom orientations & positions for plotting
@@ -71,7 +69,7 @@ N_subsample = 25
 orient_odom_subset = orient_odom[::N_subsample] 
 pos_odom_subset = pos_odom[::N_subsample]
 
-orient_dir_0 = np.array([1,0,0]) # starting vector pointing in x-direction
+
 
 # apply orient_odom quaternion rotations to orient0
 orient_odom_dir = []
@@ -81,19 +79,30 @@ for i in range(orient_odom_subset.shape[0]):
 orient_odom_dir = np.array(orient_odom_dir)
 
 # IMU
+
+# Subtract bias 
+theta_imu_dot_unbiased = removeBias(theta_imu_dot)
+
 # Convert angular velocity into angle by integrating
 dt = 0.02
-theta_imu_ = np.cumsum(theta_imu_dot*dt)
+theta_imu_ = np.cumsum(theta_imu_dot_unbiased*dt)
 
 
 # --- Synchronize Measurements ---
-
 # synchronize measurement start
-pos_gt_, theta_imu_ = synchronizeTimeOffsets(pos_gt_, theta_imu_, pos_imu_dot)
+idx_start_gt, idx_start_imu = getTimeSynchIndices(pos_gt_aligned, lin_accel_imu, plot=PLOT_STARTPOINT_DETECTION)
 
 # get synchronized subsamples of 50Hz and 120Hz measurements
-pos_gt_synched, theta_imu_synched = synchronizeRates(pos_gt_, theta_imu_)
+pos_gt_synched, theta_imu_synched = synchronizeRates(pos_gt_aligned[idx_start_gt: ], 
+                                                     theta_imu_[idx_start_imu: ],
+                                                     N_subsample=2)
 
+
+
+# --- Print some stats -----------------------------------------------------------------
+print("--- Data Stats -------------------------------------------------------")
+print("Distance travelled according to gt (aligned): \t", round(getDistTraveled(pos_gt_aligned),5),"m")
+print("Distance travelled according to odom: \t\t", round(getDistTraveled(pos_odom),5),"m")
 
 
 
@@ -102,44 +111,58 @@ fig = plt.figure()
 ax = fig.add_subplot()
 
 ## --- Positions ---
-
 # plot ROS system pose
 if PLOT_POS_ODOM == 1:
-    ax.scatter(pos_odom[:, 0], 
-            pos_odom[:, 1], 
-            s=0.1, c='blue',
-            label='Odom') 
+    ax.scatter( pos_odom[:, 0], 
+                pos_odom[:, 1], 
+                s=0.1, c='blue',
+                label='Odom') 
 
 # plot ground truth from OptiTrack system
 if PLOT_POS_GT_RAW:
-    ax.scatter(pos_gt[gt_startFrame:, 0], 
-            pos_gt[gt_startFrame:, 1],
-            alpha=0.15, 
-            s=0.1, c='black',
-            label='OptiTrack GT raw')
+    ax.scatter( pos_gt[gt_startFrame:, 0], 
+                pos_gt[gt_startFrame:, 1],
+                alpha=0.15, 
+                s=0.1, c='black',
+                label='pos gt raw')
 
 if PLOT_POS_GT_ADJ == 1:
-    ax.scatter(pos_gt_[gt_startFrame:, 0], 
-            pos_gt_[gt_startFrame:, 1], 
-            s=0.1, c='orange',
-            label='OptiTrack GT adjusted')
+    ax.scatter( pos_gt_aligned[:, 0], 
+                pos_gt_aligned[:, 1], 
+                s=0.1, c='orange',
+                label='pos gt adjusted')
+    
+    # highlight synchronization samples
+    ax.scatter( pos_gt_synched[:, 0], 
+                pos_gt_synched[:, 1], 
+                s=10, c='black',
+                label='Synch Samples',
+                zorder=10)
+    
+    # highlight time synch sample in pos data
+    ax.scatter( pos_gt_aligned[idx_start_gt, 0], 
+                pos_gt_aligned[idx_start_gt, 1], 
+                marker='+', s=30, c='red',
+                label='Time Synch Sample',
+                zorder=20)
+    
+
 
 ## --- Orientations ---
-
 # Odom orientations
 if PLOT_ODOM_ORIENTATIONS == 1:
-    ax.quiver(pos_odom_subset[:,0], pos_odom_subset[:,1], 
-            orient_odom_dir[:,0], orient_odom_dir[:,1],
-            color='blue', scale=20, 
-            label='Odom orientation')
+    ax.quiver(  pos_odom_subset[:,0], pos_odom_subset[:,1], 
+                orient_odom_dir[:,0], orient_odom_dir[:,1],
+                color='blue', 
+                label='Odom orientation')
 
 # IMU orientations
 if PLOT_IMU_ORIENTATIONS == 1:  
     theta_imu_dir = angles2vects(theta_imu_synched)        
-    ax.quiver(pos_gt_synched[:,0], pos_gt_synched[:,1], 
-        theta_imu_dir[:,0], theta_imu_dir[:,1],
-        color='blue', scale=20, 
-        label='IMU orientation')
+    ax.quiver(  pos_gt_synched[:, 0], pos_gt_synched[:, 1], 
+                theta_imu_dir[1:, 0], theta_imu_dir[1:, 1],
+                color='blue', 
+                label='IMU orientation')
 
 ax.legend()
 ax.grid(True)
@@ -147,3 +170,4 @@ ax.axis('equal')
 plt.show(block = False)
 
 dummy = 1
+
--- _doc/DataViz/dataViz_utils.py
+++ _doc/DataViz/dataViz_utils.py
@@ -1,10 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 from rosbags.rosbag2 import Reader
 from rosbags.typesys import Stores, get_typestore
 
 import csv
 
-# --- RosBag Reader util -----------------------------------------------------
+
+# --- Data reader utils -----------------------------------------------------
 
 def readOdomData(rosbag_path):
 
@@ -32,7 +35,6 @@ def readOdomData(rosbag_path):
 
     return np.array(pos), np.array(orient)
 
-
 def readImuData(rosbag_path, raw=True):
 
     reader = Reader(rosbag_path) 
@@ -59,16 +61,13 @@ def readImuData(rosbag_path, raw=True):
                 x_dot.append(msg.linear_acceleration.x) 
                 y_dot.append(msg.linear_acceleration.y) 
 
-        pos_imu_dot = np.hstack((
+        lin_accel_imu = np.hstack((
             np.array(x_dot)[:,np.newaxis],
             np.array(y_dot)[:,np.newaxis]
         ))
         
-    return np.array(theta_dot), pos_imu_dot
+    return np.array(theta_dot), lin_accel_imu
 
-
-
-# --- OptiTrack CSV Reader util ------------------------------------------------
 def readOptiTrackCSV(optitrack_csv_path):
     """ 
         Reads from (headerless) OptiTrack Motive CSV rigid body output.
@@ -89,6 +88,105 @@ def readOptiTrackCSV(optitrack_csv_path):
     pos = np.array(pos).astype('float')
     return pos, orient
 
+
+# --- Measurement synchronization --------------------------------------------
+
+def getTimeSynchIndices(pos_gt, lin_accel_imu, gt_threshold=0.0002, imu_threshold=0.005, plot=False):
+    """
+        Finds index for point in time, where car starts driving in the two different
+        measurement systems:
+        
+        pos_gt:      Positions from OptiTrack system to synchronize
+        theta_imu:   Integrated angles from IMU to synchronize
+        lin_accel_imu: Linear xy-velocities from IMU to locate drive start in
+    """
+    
+    # Find index for first position change in pos_gt
+    pos_gt_dot = pos_gt[1:]-pos_gt[0:-1]
+    v_length_gt = np.linalg.norm(pos_gt_dot, axis=1)[1:]
+
+    idx_start_gt = np.argmax(v_length_gt>gt_threshold) -1
+
+    if plot == True:
+        # show chosen start index for pos_gt
+        plt.figure(0)
+        plt.title("Finding drive start in pos_gt_dot; threashold= {}".format(gt_threshold))
+        plt.plot(v_length_gt)
+        plt.vlines(idx_start_gt, np.min(v_length_gt), np.max(v_length_gt))
+        plt.hlines(gt_threshold, 0, v_length_gt.shape[0])
+        plt.show(block=False)
+
+    # Find index for first linear_velocity change over threashold
+    v_length_imu = np.linalg.norm(lin_accel_imu, axis=1)
+    v_length_imu = v_length_imu-v_length_imu[0]
+
+    idx_start_imu = np.argmax(v_length_imu>imu_threshold)
+
+    if plot == True:
+        # show chosen start index for theta_imu
+        plt.figure(1)
+        plt.title("Finding drive start in lin_accel_imu; threashold= {}".format(imu_threshold))
+        plt.plot(v_length_imu)
+        plt.vlines(idx_start_imu, np.min(v_length_imu), np.max(v_length_imu))
+        plt.hlines(imu_threshold, 0, v_length_imu.shape[0])
+        plt.show(block=False)
+
+    return idx_start_gt, idx_start_imu
+
+
+def synchronizeRates(pos_gt, theta_imu, N_subsample):
+    """ 
+        Subsamples OptiTrack gt positions and IMU orientations (theta) with
+        harmonic subsampling rates, in order to get synchronized measurements
+
+        pos_gt:      OptiTrack 120Hz position array
+        theta_imu:   IMU 50Hz z-rotation array
+        N_subsample: Subsampling of _synchronized_ arrays
+
+        return: pos_gt_synched, theta_imu_synched
+    """
+    # subsample harmonics
+    pos_gt_synched = pos_gt[::12*N_subsample]     
+    theta_imu_synched = theta_imu[::5*N_subsample] 
+
+    # clip to shorter measurement series
+    clip_idx = min(pos_gt_synched.shape[0], theta_imu_synched.shape[0])
+
+    return pos_gt_synched[0:clip_idx], theta_imu_synched[0:clip_idx]
+
+
+
+# --- Stats ---------------------------------------------------------------------
+def getLocalStats(signal, windowSize):
+    N = len(signal)
+    m = windowSize
+
+    mean = []
+    std = []
+    
+    for i in range(N-(m-1)): # i is starting index of window
+       x = signal[i:i+m]
+       mean.append(np.mean(x))
+       std.append(np.std(x)) 
+
+    return np.array(mean), np.array(std)
+
+def removeBias(x, plot=False):
+    m, s = getLocalStats(x, windowSize=5)
+    idleIdx = np.argmin(s)
+
+    if plot == True:
+        plt.figure()
+        plt.plot(s)
+        plt.vlines(idleIdx, 0, np.max(s))
+
+    return x-m[idleIdx]
+
+def getDistTraveled(posArray):
+    path_lengths = np.linalg.norm(posArray[1:]-posArray[:-1], axis=1)
+    dist = np.sum(path_lengths)
+    return dist
+    
 
 # --- Quaternion util ---------------------------------------------------------------
 def quaternionRotPoint(p, q):
@@ -128,6 +226,7 @@ def getQuatAxis(q):
     v_ = v/np.linalg.norm(v)
     return v_
 
+
 # --- Other util -------------------------------------------------------------
 
 def angles2vects(theta):
@@ -138,50 +237,4 @@ def angles2vects(theta):
 
 def deg2rad(deg):
     return deg * (2*np.pi)/(360)
-
-
-# --- Measurement synchronization --------------------------------------------
-
-def synchronizeTimeOffsets(pos_gt, theta_imu, pos_imu_dot, gt_threshold=0.0001, imu_threshold=0.005):
-    """
-        Finds index for point in time, where car starts driving in the two different
-        measurement systems:
-        
-        pos_gt:      Positions from OptiTrack system to synchronize
-        theta_imu:   Integrated angles from IMU to synchronize
-        pos_imu_dot: Linear xy-velocities from IMU to locate drive start in
-    """
-    
-    # Find index for first position change in pos_gt
-    dummy = 1
-    x_dot_gt = pos_gt[1:, 0]-pos_gt[0:-1, 0]
-    idx_start_gt = np.argmax(x_dot_gt>gt_threshold) # argmax returns index of first 'True'
-
-    # Find index for first x
-    v_length = np.linalg.norm(pos_imu_dot, axis=1)
-    idx_start_imu = np.argmax((v_length-v_length[0])>imu_threshold)
-
-    return pos_gt[idx_start_gt:], theta_imu[idx_start_imu:]
-
-
-def synchronizeRates(pos_gt, theta_imu, N_subsample=5):
-    """ 
-        Subsamples OptiTrack gt positions and IMU orientations (theta) with
-        harmonic subsampling rates, in order to get synchronized measurements
-
-        pos_gt:      OptiTrack 120Hz position array
-        theta_imu:   IMU 50Hz z-rotation array
-        N_subsample: Subsampling of _synchronized_ arrays
-
-        return: pos_gt_synched, theta_imu_synched
-    """
-    # subsample harmonics
-    pos_gt_synched = pos_gt[::12*N_subsample]     
-    theta_imu_synched = theta_imu[::5*N_subsample] 
-
-    # clip to shorter measurement series
-    clip_idx = min(pos_gt_synched.shape[0], theta_imu_synched.shape[0])
-
-    return pos_gt_synched[0:clip_idx], theta_imu_synched[0:clip_idx]
-
 
